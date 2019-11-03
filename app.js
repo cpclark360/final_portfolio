75bdfea510b9679627fdd29f239cde5e9900ad33
Cedric Clark <cpclark360@gmail.com>
2019-11-02 20:04:35 -0500

    corrected runtime error path

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const path = require();
+const path = require("path");
 
 let cors = require("cors");
 
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname), "client", "build", "index.html");
+    res.sendFile(path.resolve(__dirname), "frontend", "build", "index.html");
   });
 }
 
---
+++ frontend/build/precache-manifest.c6a62445a5e4e1de097d18f41f360e6e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5d52ce319be99e482d01b9e99b6b5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a07062e5e398782a2fa",
+    "url": "/static/css/2.69ca2a4b.chunk.css"
+  },
+  {
+    "revision": "c9aeef2dd7a03c4e4e39",
+    "url": "/static/css/main.1ce8f166.chunk.css"
+  },
+  {
+    "revision": "1a07062e5e398782a2fa",
+    "url": "/static/js/2.c80d31b1.chunk.js"
+  },
+  {
+    "revision": "c9aeef2dd7a03c4e4e39",
+    "url": "/static/js/main.498ff441.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "38445e8d7100f8d3d62c71c7013a6a7c",
+    "url": "/static/media/CSS3.38445e8d.svg"
+  },
+  {
+    "revision": "742dc009e978727849f67705e7e9e3bc",
+    "url": "/static/media/GitHub.742dc009.png"
+  },
+  {
+    "revision": "8db9cd685111f6d953eb15bc5a3685fb",
+    "url": "/static/media/JS.8db9cd68.svg"
+  },
+  {
+    "revision": "41d2595aa5cee39ca4701b3404ec8af2",
+    "url": "/static/media/codepen.41d2595a.png"
+  },
+  {
+    "revision": "23492d49eefc1794c50377c2613baa00",
+    "url": "/static/media/dev-logo.23492d49.jpg"
+  },
+  {
+    "revision": "99325d4efd59006419033a0523a5b0ba",
+    "url": "/static/media/html5.99325d4e.svg"
+  },
+  {
+    "revision": "c3fcc390427d68b89647e834adf7fc87",
+    "url": "/static/media/linkedin.c3fcc390.png"
+  },
+  {
+    "revision": "357b6da991bba1c60c8134200cbca4c4",
+    "url": "/static/media/me.357b6da9.jpg"
+  },
+  {
+    "revision": "cdc525fef1db60b5ba5c2524d80a06ce",
+    "url": "/static/media/node.cdc525fe.svg"
+  },
+  {
+    "revision": "d8cf77403db1c802576acc37627ff00b",
+    "url": "/static/media/react.d8cf7740.svg"
+  },
+  {
+    "revision": "e4730e372844c5c90a62067f376c0f4b",
+    "url": "/static/media/repl.e4730e37.svg"
+  },
+  {
+    "revision": "585e4c638d69da2761733ec40f5f8bc3",
+    "url": "/static/media/todo.585e4c63.png"
+  },
+  {
+    "revision": "8445dfa8dcb02896fb16762402538385",
+    "url": "/static/media/twitter.8445dfa8.png"
+  }
+]);
